5545bb2778a52227536343048026986ac643e4c5
ndthanh-114 <66512929+ndthanh-114@users.noreply.github.com>
2021-07-17 21:02:07 +0700

    in_up

--- server/index.js
+++ server/index.js
@@ -12,8 +12,11 @@ app.use(bodyParser.urlencoded({ limit: "30mb", extended: true}))
 app.use(cors())
 
 app.use('/', userRoutes)
+app.get('/', (req, res) => {
+    res.send('App is running')
+})
 
-const CONNECTION_URL= 'mongodb://localhost:27017/signin_signup'
+const CONNECTION_URL= 'mongodb+srv://admin:admin123@cluster0.sblvb.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
 const PORT = process.env.PORT || 5000
 
 mongoose.connect(CONNECTION_URL, {useNewUrlParser: true, useUnifiedTopology: true})
